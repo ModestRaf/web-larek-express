3b98a0ff713180f94e19d2ae9ab410308df2659c
ModestRaf <tanya.stargate@yandex.ru>
2025-05-25 21:34:33 +0300

    настроена работа сервера

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Server with CORS is working!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server with CORS started on http://localhost:${PORT}`);
+});
